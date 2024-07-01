e25fda0ab5f7c99cd47ec07a25713c9798bd0eab
Rehan1337 <tencentgaming991@gmail.com>
2024-07-01 14:10:44 +0500

    First

---
+++ script.js
@@ -0,0 +1,11 @@
+const myPara = document.getElementById("your-para");
+myPara. innerHTML = "‹b›Pakistan Zindabad</b>"; setInterval(function (
+let color = Math. floor (Math. random (
+*16777215
+- toString(16);
+26777215)
+- toString(16);
+let color2 = Math.floor (Math. random )
+myPara.style.backgroundColor = "#" * color1;
+myPara.style.color = "#* + color2;
+｝， 509） ；
